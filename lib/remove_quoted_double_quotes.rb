2b1831ad077ef72aea564e2bec669fe62914c6d9
Chris Parsons <chris@thinkcodelearn.com>
2012-11-16 09:49:59 +0000

    test output to check possible transformations

---
+++ lib/remove_quoted_double_quotes.rb
@@ -0,0 +1,19 @@
+QUOTE = '"\u201C\u201D\u201E\u201F\u2033\u2036'
+
+Edition.all.each do |edition|
+  affected = false
+  new_body = edition.body.lines.map do |line|
+    if line.match(/^> /)
+      if line.match(/[#{QUOTE}]/)
+        new_line = line.sub(/[#{QUOTE}]/, '')
+        affected = true
+        puts "#{line} => #{new_line}"
+        new_line
+      else
+        line
+      end
+    else
+      line
+    end
+  end.join('\n')
+end
