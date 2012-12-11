4bd8271140e70e4d44c0a8541324ebc2e06a1578
James Stewart <jys@ketlai.co.uk>
2012-12-11 10:35:06 +0000

    Sketch of API controller/presenter for generic editions API
    This is primarily for exploration to kick start a session working out what will
    fit in a mainstream-like API and what won't.

---
+++ app/controllers/api/other_editions_controller.rb
@@ -0,0 +1,42 @@
+class Api::OtherEditionsController < PublicFacingController
+  respond_to :json
+
+  self.responder = Api::Responder
+
+  def show
+    @document = document_published_as(params[:id])
+
+    if @document
+      respond_with Api::GenericEditionPresenter.new(@document)
+    else
+      render text: "Not found", status: :not_found
+    end
+  end
+
+  def index
+    respond_with Api::DetailedGuidePresenter.paginate(
+      DetailedGuide.published.alphabetical
+    )
+  end
+
+  def tags
+    @results = MainstreamCategory.with_published_content.where(parent_tag: params[:parent_id])
+    if @results.any?
+      respond_with Api::MainstreamCategoryTagPresenter.new(@results)
+    else
+      respond_with_not_found
+    end
+  end
+
+  private
+
+  def document_published_as(slug)
+    document = Document.where(slug: slug).first
+    document && document.published_edition
+  end
+
+
+  def respond_with_not_found
+    respond_with Hash.new, status: :not_found
+  end
+end
